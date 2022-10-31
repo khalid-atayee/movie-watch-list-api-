307da0bff0f7ed6b5a5ca1301f3be4060180eb6a
khalid-atayee <khalidatayee.76@gmail.com>
2022-10-31 16:08:01 +0430

    movies watchlist is done some extra feactures are needed for better performance like duplicate movies should not be added to watch list, i will work on it when i get time

---
+++ homeScript.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', bringData);
+let watchlistContainer = document.getElementById('watchlist-container');
+function bringData(){
+    watchlistContainer.innerHTML=''
+    render()
+
+}
+
+
+
+function render() {
+    // ${JSON.stringify(movie).replace(/"/g,'\&quot;')}
+    let datas = JSON.parse(localStorage.getItem('movies'))
+    datas.map((movie) => {
+        watchlistContainer.innerHTML += `<img src="${movie.Poster}" alt="">
+            <div class="movie">
+                <div class="movie-title">
+                    <p>${movie.Title}</p>
+                    <p class="star"><i class="fa-solid fa-star"></i> ${movie.imdbRating} </p>
+                </div>
+                <div class="movie-details">
+                    <p class="genre">${movie.Runtime}</p>
+                    <p class="genre">${movie.Genre}</p>
+                    <p onclick="remove(${JSON.stringify(movie).replace(/"/g,'\&quot;')})" class="genre addCart"><i class="fa-solid fa-circle-plus"></i> remove</p>
+                </div>
+                <div class="movie-description">
+                    <p>${movie.Plot}</p>
+
+                </div>
+
+            </div>
+            
+            `
+    })
+
+}
+
+function remove(data){
+    datas =JSON.parse(localStorage.getItem('movies'))
+
+    let finalData = datas.filter((singleData)=>{
+        return JSON.stringify(singleData)!==JSON.stringify(data);
+    })
+
+    localStorage.removeItem('movies')
+    localStorage.setItem('movies',JSON.stringify(finalData))
+    bringData();
+}
+
+
+
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
 const submitForm = document.getElementById('search-form');
-let movieContainer= document.querySelector('.movie-container');
+let searchContainer= document.getElementById('search-container');
+
 let values=[],titles,html;
 
 submitForm.addEventListener('submit', (e)=>{
     count=0;
-    movieContainer.innerHTML='';
+    searchContainer.innerHTML='';
     e.preventDefault();
     let formdata = new FormData(submitForm)
     value = formdata.get('movie-search')
@@ -17,7 +18,7 @@ submitForm.addEventListener('submit', (e)=>{
          
             fetch(`http://www.omdbapi.com/?i=${value.imdbID}&apikey=2dc7cc4b`)
             .then(Response=>Response.json())
-            .then(data=>render(data))
+            .then(data=>renderSearch(data))
         })
         document.getElementById('search-input').value='';
 
@@ -26,11 +27,11 @@ submitForm.addEventListener('submit', (e)=>{
     })
 })
 
-function render(movie){
-    let data = movie;
-    data = JSON.stringify(data);
+function renderSearch(movie){
+    // let data = movie;
+   
     // console.log(data)
-    movieContainer.innerHTML += `<img src="${movie.Poster}" alt="">
+    searchContainer.innerHTML += `<img src="${movie.Poster}" alt="">
     <div class="movie">
         <div class="movie-title">
             <p>${movie.Title}</p>
@@ -39,7 +40,7 @@ function render(movie){
         <div class="movie-details">
             <p class="genre">${movie.Runtime}</p>
             <p class="genre">${movie.Genre}</p>
-            <p data-check="${data}" data-id=movie${count++} class="genre addCart"><i class="fa-solid fa-circle-plus"></i> Watchlist</p>
+            <p onclick="setToLocalStorage(${JSON.stringify(movie).replace(/"/g,'\&quot;')})" class="genre addCart"><i class="fa-solid fa-circle-plus"></i> Watchlist</p>
         </div>
         <div class="movie-description">
             <p>${movie.Plot}</p>
@@ -49,15 +50,26 @@ function render(movie){
     </div>
     
     `
-   
-   
+}
+
+function setToLocalStorage(data){
+  
+    let validJSON = JSON.parse(localStorage.getItem("movies") || "[]");
+    validJSON.push(data)
+    localStorage.setItem('movies',JSON.stringify(validJSON))
+    // console.log(validJSON)
+    // validJSON.map((data)=>{
+    //     renderWatchlist(data)
+    // })
+ 
+
 }
 
 
-document.addEventListener('click',(e)=>{
-    if(e.target.dataset.id==document.getElementsByName('p').dataset.id){
-        console.log('ok')
-    }
-   
 
-})
+
+
+
+
+
+
